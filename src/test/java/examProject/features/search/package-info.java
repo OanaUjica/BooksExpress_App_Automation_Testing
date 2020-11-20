8647eb67f9e832a0ae70adcf1f92a6ed421336a6
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2020-11-20 10:58:31 +0200

    Added logic for wishlist and cart

--- src/test/java/examProject/features/search/package-info.java
+++ src/test/java/examProject/features/search/package-info.java
@@ -3,7 +3,7 @@
         title = "BooksExpress online library",
         text = {"In order to buy a book",
                 "As a user",
-                "I want to be able to login, search for the book, add it to wishlist and cart"},
+                "I want to be able to login, search for the book, add it to wishlist and cart and then to logout"},
         cardNumber = "#123"
 )
 package examProject.features.search;
